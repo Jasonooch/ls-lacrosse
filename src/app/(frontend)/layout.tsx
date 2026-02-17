7a9fcaa62eabb9a317ee194651bb7e76bc796d94
Jasonooch <jasonorlando14@gmail.com>
2026-02-17 12:48:07 -0500

    Update frontend layout metadata

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,31 +1,28 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Footer from "@/components/blocks/Footer";
-import Header from "@/components/blocks/Header";
-import MobileNav from "@/components/blocks/MobileNav";
+import type { Metadata } from 'next'
+import './globals.css'
+import Footer from '@/components/blocks/Footer'
+import Header from '@/components/blocks/Header'
+import MobileNav from '@/components/blocks/MobileNav'
 
 export const metadata: Metadata = {
-  title: "LS Lacrosse - Lancaster-Lebanon League Champions",
-  description: "Official website for LS Lacrosse - Stay updated with game schedules, team rosters, news, and achievements from Lancaster-Lebanon League champions.",
-};
-
+  title: 'LS Lacrosse',
+  description:
+    'Official website for LS Lacrosse - Stay updated with game schedules, team rosters, news, and achievements.',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
-
   return (
     <html lang="en">
       <body className="antialiased">
         <Header />
         <MobileNav />
-        <main>
-          {children}
-        </main>
+        <main>{children}</main>
         <Footer />
       </body>
     </html>
-  );
+  )
 }
